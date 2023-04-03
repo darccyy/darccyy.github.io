fc0b66455bbf1a967be189f3da08c05209e92689
darcy <darcy3057@gmail.com>
2023-04-03 17:21:09 +1000

    Make good

--- README.md
+++ README.md
@@ -1,27 +1,3 @@
-# Unreact App Template
+# My Website
 
-A basic template for an [Unreact](https://crates.io/crates/unreact) SSG app in Rust.
-
--   [Live on GitHub Pages](https://darccyy.github.io/unreact-template)
--   [Unreact documentation](https://docs.rs/unreact/latest/unreact)
--   [Unreact template source](https://github.com/darccyy/unreact-template)
-
-This template uses the latest stable version of Unreact.
-
-# Usage
-
-```bash
-# Clone the repo
-git clone https://github.com/darccyy/unreact-template my-unreact-app
-cd my-unreact-app
-
-# Run in dev mode
-# Serves to localhost:3000, reloads client on file changes
-just serve
-
-# Compile in production mode
-# This is in the GitHub action
-cargo run --no-default-features
-```
-
-![Unreact Icon](./public/icon.png)
+[Here](https://darccyy.github.io)
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,21 @@
 use unreact::prelude::*;
 
 // Where the site is hosted
-const URL: &str = "https://darccyy.github.io/unreact-template";
+const URL: &str = "https://darccyy.github.io/";
 
 fn main() -> Result<(), Error> {
-    let mut app = Unreact::new(Config::default(), is_dev(), URL)?;
+    let config = Config {
+        strict: true,
+        ..Config::default()
+    };
+
+    let mut app = Unreact::new(config, is_dev(), URL)?;
 
     app
         // Index page
-        .index("index", object! { secret: "Hello!" })
-        // 404 page
-        .not_found("404", object! {});
-
-    // Custom page
-    app.route(
-        "hello/there",
-        "hello",
-        object! { list: vec![1, 2, 3, 4, 5] },
-    );
-
-    // Complete app
-    app.run()?;
+        .index("homepage", object! { secret: "Hello!" })
+        // Complete app
+        .run()?;
 
     // Only prints if NOT in dev mode
     println!("Compiled for production.");
